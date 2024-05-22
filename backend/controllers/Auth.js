6fb80764445d77d0af8ff059f6dbc50ee7179961
milan-bavishi <milanbavishi99@gmail.com>
2024-05-22 10:34:55 +0530

    Login Added

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -102,6 +102,34 @@ exports.signup = async (req, res) => {
   }
 };
 
+exports.login = async (req,res) => {
+    try{
+
+        const{ email, password } =  req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: `Plese Fill up All the Requied Fields`,
+            });
+        }
+
+        const user = await User.findOne({ email }).populate("additionDetails");
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: `User is not Registered with Us Plese SignUp to Continue`,
+            });
+        }
+
+        if(await bcrypt.compare(password, user.password)){
+            
+        }
+    }
+};
+
+
 exports.sendotp = async (req, res) => {
   try {
     const { email } = req.body;
