924e2c70da9811e619a1be83aecf528202516e18
milan-bavishi <milanbavishi99@gmail.com>
2024-05-30 07:07:13 +0530

    Mail Templates Updated

--- backend/mail/templates/paymentSuccess.js
+++ backend/mail/templates/paymentSuccess.js
@@ -67,7 +67,7 @@ exports.paymentSuccess = (amount, paymentId, orderId, name,lastname) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+            <a href=""><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
                     alt="StudyNotion Logo"></a>
             <div class="message">
             Your payment of ₹${amount} has been successfully received.
@@ -83,7 +83,7 @@ exports.paymentSuccess = (amount, paymentId, orderId, name,lastname) => {
                 </p>
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                    href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                    href="mailto:team.studynotion@gmail.com">team.studynotion@gmail.com</a>. We are here to help!</div>
         </div>
     </body>
     
