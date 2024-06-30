17d0f93bd858d51f70e779fba0d37b915b8776ab
milan-bavishi <milanbavishi99@gmail.com>
2024-06-30 18:37:27 +0530

    Reset password link update

--- backend/controllers/ResetPassword.js
+++ backend/controllers/ResetPassword.js
@@ -30,7 +30,7 @@ exports.resetPasswordToken = async (req,res) => {
 
         console.log("DETAILS",updatedDetails);
 
-        const url = `http://localhost:3000/update-password/${token}`
+        const url = `https://studynotion-edtech-milan.vercel.app/update-password/${token}`
 
         await mailSender(
             email,
